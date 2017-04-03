65bc5e5b687ebb2d21c94c5b41b7df1c3631d6c6
stoneWangL <stoneWang_L@163.com>
2017-04-03 18:46:16 +0800

    完成五子棋

--- app/src/main/java/com/example/stonewang/wuzhiqi/MainActivity.java
+++ app/src/main/java/com/example/stonewang/wuzhiqi/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.stonewang.wuzhiqi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WuzhiqiPanel wuzhiqiPanel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wuzhiqiPanel = (WuzhiqiPanel) findViewById(R.id.id_wuziqi);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            wuzhiqiPanel.start();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
